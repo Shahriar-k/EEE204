4c94e4cd14e356a50d981b9d061472052c576ae6
Shahriar Khan <67644471+Shahriar-k@users.noreply.github.com>
2021-03-24 21:53:35 +0600

    Create ex_5_3b.m

---
+++ Bracketing Method/ex_5_3b.m
@@ -0,0 +1,25 @@
+clc 
+
+xl = 0.5;
+xu = 1;
+
+for k=1:2
+   xr = (xl+xu)/2;
+   
+   fxu = -25+82*xu-90*xu^2+44*xu^3-8*xu^4+0.7*xu^5;
+   fxl = -25+82*xl-90*xl^2+44*xl^3-8*xl^4+0.7*xl^5;
+   fxr = -25+82*xr-90*xr^2+44*xr^3-8*xr^4+0.7*xr^5;
+   
+   if fxr*fxu < 0
+       xl = xr;
+   else
+       xu = xr;
+   end
+   
+   
+   xr_new = (xl+xu)/2;
+   
+   error = abs((xr_new - xr)/xr)*100;
+   
+   
+end
