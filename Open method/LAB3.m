a90a073d9ada49504c3594fb51315bd19dcefc2e
Shahriar Khan <67644471+Shahriar-k@users.noreply.github.com>
2021-04-06 22:57:48 +0600

    Create LAB3.m

---
+++ Open method/LAB3.m
@@ -0,0 +1,5 @@
+# EEE 204: Numerical Analysis for Electrical Engineering
+### Experiment 03: Root Finding Using Open Method
+## PROBLEM TO SOLVE:
+Say, you are charging an RC-storage from a source VS(t). The amount of charge stored after time
+t is given by:
